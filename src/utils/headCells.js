41924e371d4f32f7ac5730ce826b4d7c82e1be2b
Alonso Parra <alonsogparra@gmail.com>
2022-12-24 14:44:23 -0400

    headCells built

---
+++ src/utils/headCells.js
@@ -0,0 +1,17 @@
+export const headCells = [
+  {
+    id: 'name',
+    numeric: false,
+    label: 'Name',
+  },
+  {
+    id: 'stories',
+    numeric: true,
+    label: 'Stories #',
+  },
+  {
+    id: 'series',
+    numeric: true,
+    label: 'Series #',
+  },
+];
